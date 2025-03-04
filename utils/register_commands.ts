eab297f4d6f90b3306f787e13fff5ad17fed8c62
c7e715d1b04b17683718fb1e8944cc28 <ikki17617@gmail.com>
2025-03-04 15:46:05 +0900

    refactor(utils/register_commands.ts): improve code readability and structure by using async/await
    The code was refactored to use async/await for better readability and to handle potential errors more gracefully.  The command registration is now encapsulated within an async function, improving the overall structure.

--- utils/register_commands.ts
+++ utils/register_commands.ts
@@ -18,68 +18,72 @@ if (process.env.DISCORD_PUBLIC_KEY === undefined) {
 	throw new Error("DISCORD_PUBLIC_KEY is not defined");
 }
 
-const rest = ky.create({
-	prefixUrl: "https://discord.com/api/v10",
-	throwHttpErrors: true,
-});
+const main = async () => {
+	const rest = ky.create({
+		prefixUrl: "https://discord.com/api/v10",
+		throwHttpErrors: true,
+	});
 
-const json: RESTPutAPIApplicationCommandsJSONBody = [
-	{
-		name: "hoyolab_login",
-		name_localizations: {
-			ja: "hoyolabログイン",
-		},
-		type: ApplicationCommandType.ChatInput,
-		description: "Log in to HoYoLAB via Discord",
-		description_localizations: {
-			ja: "HoYoLABにDiscord経由でログイン",
-		},
-		options: [
-			{
-				type: ApplicationCommandOptionType.String,
-				name: "account",
-				name_localizations: {
-					ja: "アカウント",
-				},
-				description: "Your HoYoLAB username or email",
-				description_localizations: {
-					ja: "HoYoLABのユーザー名またはメールアドレス",
-				},
-				required: true,
-				min_length: 1,
-				max_length: 100,
+	const json: RESTPutAPIApplicationCommandsJSONBody = [
+		{
+			name: "hoyolab_login",
+			name_localizations: {
+				ja: "hoyolabログイン",
 			},
-			{
-				type: ApplicationCommandOptionType.String,
-				name: "password",
-				name_localizations: {
-					ja: "パスワード",
-				},
-				description: "Your HoYoLAB account password",
-				description_localizations: {
-					ja: "HoYoLABのアカウントのパスワード",
-				},
-				required: true,
-				// reference: https://account.hoyolab.com/login-platform/index.html?st=https%3A%2F%2Fwww.hoyolab.com%2Fsignup&token_type=6&client_type=4&app_id=c9oqaq3s3gu8&game_biz=bbs_oversea&lang=ja-jp&theme=dark-hoyolab&ux_mode=popup&iframe_level=1#/email-register
-				min_length: 8,
-				max_length: 30,
+			type: ApplicationCommandType.ChatInput,
+			description: "Log in to HoYoLAB via Discord",
+			description_localizations: {
+				ja: "HoYoLABにDiscord経由でログイン",
 			},
-		],
-	},
-];
-
-const response = await rest.put(
-	Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
-	{
-		headers: {
-			Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+			options: [
+				{
+					type: ApplicationCommandOptionType.String,
+					name: "account",
+					name_localizations: {
+						ja: "アカウント",
+					},
+					description: "Your HoYoLAB username or email",
+					description_localizations: {
+						ja: "HoYoLABのユーザー名またはメールアドレス",
+					},
+					required: true,
+					min_length: 1,
+					max_length: 100,
+				},
+				{
+					type: ApplicationCommandOptionType.String,
+					name: "password",
+					name_localizations: {
+						ja: "パスワード",
+					},
+					description: "Your HoYoLAB account password",
+					description_localizations: {
+						ja: "HoYoLABのアカウントのパスワード",
+					},
+					required: true,
+					// reference: https://account.hoyolab.com/login-platform/index.html?st=https%3A%2F%2Fwww.hoyolab.com%2Fsignup&token_type=6&client_type=4&app_id=c9oqaq3s3gu8&game_biz=bbs_oversea&lang=ja-jp&theme=dark-hoyolab&ux_mode=popup&iframe_level=1#/email-register
+					min_length: 8,
+					max_length: 30,
+				},
+			],
 		},
-		json,
-	},
-);
+	];
 
-console.log(
-	response.ok
-		? "Successfully registered commands"
-		: "Failed to register commands",
-);
+	const response = await rest.put(
+		Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+		{
+			headers: {
+				Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+			},
+			json,
+		},
+	);
+
+	console.log(
+		response.ok
+			? "Successfully registered commands"
+			: "Failed to register commands",
+	);
+};
+
+main();
